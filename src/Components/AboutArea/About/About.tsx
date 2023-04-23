0cfab673618693d9a5cd84ac8d938d8f3a133c13
Darya Shelever <d.shelever@gmail.com>
2023-04-23 14:06:08 +0300

    end

--- src/Components/AboutArea/About/About.tsx
+++ src/Components/AboutArea/About/About.tsx
@@ -1,9 +1,29 @@
 import "./About.css";
 
+import photo from "../../../Assets/photo.jpg"
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 function About(): JSX.Element {
     return (
         <div className="About">
-			about me...
+            <h1>About Me </h1>
+            <div className="me">
+               <img className="photo" src={photo} alt=""/>
+			
+            <p>My name is Darya Shelever,
+             I am 23 years old,
+              and I have completed a Full Stack Developer course. 
+              I worked on a coupon management system project, handling both backend and frontend development.
+               My responsibilities included creating and managing the database, developing server logic, and implementing the client-side interface. 
+               I have experience with technologies such as JavaScript, React, and Node.js. 
+               In the coupon management system project, I used these technologies to develop the backend part of the system using Java and Spring. 
+               Additionally, I have experience working with MySQL databases, which were used in the project to store information about coupons and customers.
+               </p> 
+            </div>
+            <div className="navbar-link">
+            <a href="https://github.com/DaryaShelever"><FaGithub/></a>
+            <a href="https://www.linkedin.com/in/darya-shelever/"><FaLinkedin/></a>
+
+            </div>
         </div>
     );
 }
--- src/Components/HomeArea/Home/Home.tsx
+++ src/Components/HomeArea/Home/Home.tsx
@@ -1,8 +1,34 @@
+import "./Home.css"
 
 function Home(): JSX.Element {
     return (
         <div className="Home">
-           <img src={'https://i.pinimg.com/564x/00/d3/e8/00d3e8c3b6149614367472d65cb40c6f.jpg'} alt={'heh'}/>
+           {/* <img src={'https://i.pinimg.com/564x/00/d3/e8/00d3e8c3b6149614367472d65cb40c6f.jpg'} alt={'heh'}/> */}
+            <h1 className="description">  Description of the project:  </h1>
+            <p className="main Box">The project is a coupon management system that allows companies to create and manage coupons, and customers to purchase them. The system has restrictions on the quantity and validity period of coupons, as well as the number of coupons each customer can purchase. </p>
+            <div className="roles ">
+                <div className="levels">
+                The system provides different levels of access depending on the user's role:
+                </div>
+            <   div className="role ">
+                    <div className="role-1">
+                    <h3>Admin </h3>
+                    <p>An administrator can manage lists of companies and customers.</p>
+                </div>
+                <div  className="role-1">
+                    <h3>Company</h3>
+                    <p>Companies can create and manage lists of coupons associated with their company.</p>
+                
+                </div>
+                <div  className="role-1">
+                    <h3>Customer</h3>
+                    <p> Customers can view and purchase coupons, as well as view their purchase history.</p>
+                </div>
+                </div>
+            </div>
+            <div className="end Box">
+                As a result, the project represents an effective solution for coupon management that is convenient for both companies and customers.
+            </div>
         </div>
     );
 }
